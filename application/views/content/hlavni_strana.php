ea8eb2ece434bd0e1669bb52a489670b0a5b3e2a
vacularadek <vacula.radek@email.cz>
2020-10-18 21:49:24 +0200

    first commit

---
+++ application/views/content/hlavni_strana.php
@@ -0,0 +1,5 @@
+
+<div class="container obsah">
+    <img src="<?php echo base_url()?>assets/moje_slozka/img/pozadi.jpg" width="100%" height="100%">
+</div>
+  
---
+++ application/views/layout/footer.php
@@ -0,0 +1,4 @@
+            <div class="footer" align="center"><a style="color: white" class="nav-link" href="<?php echo base_url()?>/assets/vlastni_soubory/dokumentace/dokumentace.pdf">Dokumentace</a></div>
+        </div>
+    </body>
+</html>
